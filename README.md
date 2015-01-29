bbd43795100d5d16b263b8c3b9e8decbf048a76d
Prakhar Srivastav <prakhar1989@gmail.com>
2015-01-29 17:39:28 +0300

    Added first 20 solutions

--- README.md
+++ README.md
@@ -28,9 +28,3 @@ FIXME: listing of options this app accepts.
 ### That You Think
 ### Might be Useful
 
-## License
-
-Copyright © 2014 FIXME
-
-Distributed under the Eclipse Public License either version 1.0 or (at
-your option) any later version.
--- src/foreclj/core.clj
+++ src/foreclj/core.clj
@@ -1,69 +1,6 @@
 (ns foreclj.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
-
-(def asym-hobbit-body-parts [{:name "head" :size 3}
-                             {:name "left-eye" :size 1}
-                             {:name "left-ear" :size 1}
-                             {:name "mouth" :size 1}
-                             {:name "nose" :size 1}
-                             {:name "neck" :size 2}
-                             {:name "left-shoulder" :size 3}
-                             {:name "left-upper-arm" :size 3}
-                             {:name "chest" :size 10}
-                             {:name "back" :size 10}
-                             {:name "left-forearm" :size 3}
-                             {:name "abdomen" :size 6}
-                             {:name "left-kidney" :size 1}
-                             {:name "left-hand" :size 2}
-                             {:name "left-knee" :size 2}
-                             {:name "left-thigh" :size 4}
-                             {:name "left-lower-leg" :size 3}
-                             {:name "left-achilles" :size 1}
-                             {:name "left-foot" :size 2}])
-
-(defn needs-matching-part?
-  [part]
-  (re-find #"^left-" (:name part)))
-
-(defn make-matching-part
-  [part]
-  {:name (clojure.string/replace (:name part) #"^left-" "right-")
-   :size (:size part)})
-
-(defn symmetrize-body-parts 
-  [asym-body-parts]
-  (reduce (fn [final-body-parts part]
-            (let [final-body-parts (conj final-body-parts part)]
-              (if (needs-matching-part? part)
-                (conj final-body-parts (make-matching-part part))
-                final-body-parts)))
-          [] asym-body-parts))
-
-;; chapter 2 
-(defn titleize
-  [topic]
-  (str topic " for the Brave and True"))
-
-;; extracting key value pairs
-(defn label-key-val
-  [[key val]]
-  (str "key: " key ", val: " val))
-
-;; map to map to vector to map
-(into {}
-      (map (fn [[key, val]] 
-             [key (inc val)]) 
-       {:max 20 :min 10}))
-
-(reduce (fn [new-map [key, val]]
-          (assoc new-map key (inc val))) 
-        {} {:max 30 :min 10})
-
 ;; fizzbuzz
 (map (fn [n] 
        (if (zero? (mod n 15)) "FizzBuzz" 
@@ -80,31 +17,6 @@
          (if (empty? result) n result)))
      (range 1 20))
 
-;; -> macro awesomeness for pipelining
-;;(-> [2 5 4 1 3 6] (reverse) (rest) (sort) (last))
-
-((fn [n key-vec] (reduce (fn [m k] (assoc m k n)) {} key-vec))
- 0 [:a :b :c])
-
-(fn [x y] 
-   (map #(Character/getNumericValue %)
-    (into [] (str (* x y)))))
-
-((fn [xs] (reduce concat (map #(take 2 (repeat %)) xs)))
- [1 2 3]) 
-
-(interleave [0 0 0] [1 2 3])
-
-(into [] (take 3 (repeat 9))) 
-
-(fn [x nums]
-  (conj (vec (interleave nums
-        (into [] (take (dec (count nums)) (repeat x)))))   
-    (last nums)))
-
-
-(#(rest (interleave (repeat %) %2)) 0 [1 2 3])
-
 (defn get-words [text] (re-seq #"\w+" (.toLowerCase text)))
 
 (defn sum
@@ -119,8 +31,109 @@
   (if (empty? vals) accumulating-total)
     (recur (rest vals) (+ (first vals) accumulating-total)))
 
-(defn factorial
-  [n]
-  (reduce * (range 2 (inc n))))
+(defn infix [v & more]
+  (reduce (fn [acc [op x]] (op acc x)) v (partition 2 more)))
 
-(factorial 5)
+(defn infix [& exprs]
+  (loop [[x & xs] exprs acc 0 oper +]
+     (cond 
+      (nil? x) acc
+      (number? x) (recur xs (oper acc x) nil)
+      :else (recur xs acc x))))
+
+(defn infix-2
+  ([x op y] (op x y))
+  ([x op y & xs]
+   (apply infix-2 (cons (infix-2 x op y) xs))))
+
+(defn gcd [x y] 
+  (if (zero? y) x (gcd y (mod x y))))
+
+(defn lcm [x y]
+  (/ (* x y) (gcd x y)))
+
+(defn answer [r1 r2]
+  (let [a (numerator r1)
+        b (denominator r1)
+        c (numerator r2)
+        d (denominator r2)]))
+
+(defn divide [a b]
+  (let [quot (count (take-while (fn [n] (<= n a)) 
+                                (iterate #(+ b %) b))) 
+        rem  (- a (* quot b))]
+    {:quot quot :rem rem }))
+
+(defn cmp [f a b]
+  (cond
+    (f a b) :lt
+    (f b a) :gt
+    :else :eq))
+
+(defn set-inter [a b]
+  (let [xs (for [x a y b] (set [x y]))
+        ys (filter #(= 1 (count %)) xs)]
+    (reduce clojure.set/union #{} ys)))
+
+(defn rev-inter [xs n]
+  (let [i (/ (count xs) n)]
+    (partition i xs)))
+
+(defn cc [chrs-seqs]
+  (let [counter (fn [acc k] (assoc acc k (inc (get acc k 0))))
+        count-char (fn [chrs] (reduce counter {} (seq chrs)))
+        get-counts (map count-char chrs-seqs)
+        counts-map (zipmap chrs-seqs get-counts)
+        anagram-map (reduce (fn [acc [k v]] 
+                                (assoc acc v (conj (get acc v []) k))) 
+                            {} counts-map)]
+      (->>
+        (vals anagram-map) (map set) (filter second) (set))))
+
+(defn get-map-count 
+  "increments count of word in provided map(counts)"
+  [counts word]
+  (assoc counts word 
+         (inc (get counts word 0))))
+
+(defn card-game [card]
+  (let [ suits {\D :diamond \H :heart \C :club \S :spade}
+         ranks {\T 8 \J 9 \Q 10 \K 11 \A 12}
+         [s r] (seq card)
+         suit (get suits s)]
+    (if-let [rank (get ranks r)]
+       {:suit suit :rank rank}
+       {:suit suit :rank (- (Character/getNumericValue r) 2)})))
+
+(defn perfect-squares [seqs]
+  (->>
+    (clojure.string/split seqs #",")
+    (map #(Integer/parseInt %))
+    (filter #(let [x (Math/sqrt %)] (== x (int x))))
+    (clojure.string/join ",")))
+
+(defn get-distinct [seqs]
+  (loop [xs seqs
+         uniq #{}]
+    (if (uniq (first xs)) 
+      (recur (rest xs) (conj uniq (first xs)))
+      (recur (rest xs) uniq))))
+
+(defn perfect-nums [n]
+  (->>
+    (range 1 n)
+    (filter #(zero? (rem n %)))
+    (reduce +)
+    (= n)))
+
+(defn to-camel-case [word]
+  (let [[x & xs] (.split word #"-")]
+    (apply str (conj (map clojure.string/capitalize xs) x))))
+
+(defn balanced-num [xs]
+  (let [xs (->> (Integer/toString xs) (seq) (map #(Character/getNumericValue %)))
+        len (/ (count xs) 2)]
+    (if (zero? (mod (count xs) 2)) 
+      (let [[a b] (split-at len xs)] (= (reduce + a) (reduce + b)))
+      (let [[a b] (split-at (dec len) xs)] (= (reduce + a) (reduce + (rest b)))))))
+
